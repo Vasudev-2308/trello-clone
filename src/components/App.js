893c3c47013ecd18a27ace2b74c795326882334c
Vasudev-2308 <kingzvasu660@gmail.com>
2023-05-04 19:18:23 +0530

    Modified Code Based On Need

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Header">React Trello Clone</div>
-
+        <div className="Header">U-Track</div>
         <Board />
+        <div className="chatbody">
+          <iframe 
+          title="ChatBot"
+          src="https://web.powerva.microsoft.com/environments/Default-6097692a-0a7b-4e97-be15-92710bdb2994/bots/new_bot_113bc765e3be427e9a14138db29d145e/webchat" frameBorder="0"></iframe>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./store";
 
-import * as serviceWorker from "./serviceWorker";
-
 import "./index.css";
 
 const rootElement = document.getElementById("root");
@@ -17,8 +15,3 @@ ReactDOM.render(
   </Provider>,
   rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
